37c10463f5a3b9ba7853f6e94361d9456e9f248f
chaight455-2 <chaight@wisc.edu>
2026-02-22 15:22:58 -0600

    Styled popups and slider implemented

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,18 +1,43 @@
-var map = L.map('map').setView([39.5, -98.35], 4);
+var map = L.map('map').setView([39.5, -98.35], 3);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+var currentYear = 1980;
+var geoJsonLayer;
+
+function buildPopup(props) {
+    var visitors = props[currentYear];
+    var visitorText = (visitors != null && visitors !== 0)
+        ? visitors.toLocaleString()
+        : 'No data for ' + currentYear;
+    return '<div class="popup-content">' +
+        '<h3>' + props.NAME + '</h3>' +
+        '<p><span class="popup-label">Year:</span> ' + currentYear + '</p>' +
+        '<p><span class="popup-label">Visitors:</span> ' + visitorText + '</p>' +
+        '</div>';
+}
+
 fetch('data/data.geojson')
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        L.geoJSON(data, {
+        geoJsonLayer = L.geoJSON(data, {
             onEachFeature: function (feature, layer) {
-                layer.bindPopup(feature.properties.NAME);
+                layer.bindPopup(buildPopup(feature.properties));
             }
         }).addTo(map);
     });
+
+document.getElementById('year-slider').addEventListener('input', function () {
+    currentYear = parseInt(this.value);
+    document.getElementById('year-display').textContent = currentYear;
+    if (geoJsonLayer) {
+        geoJsonLayer.eachLayer(function (layer) {
+            layer.setPopupContent(buildPopup(layer.feature.properties));
+        });
+    }
+});
